7cb14f4e2065116a06e217fdf52897488f9a64e8
echpochmak31 <echpochmak83@gmail.com>
2023-04-16 15:34:57 +0300

    fix: db scheme naming

--- SpringProject/migrations/scheme.sql
+++ SpringProject/migrations/scheme.sql
@@ -3,22 +3,22 @@
 --changeset gleb:1
 CREATE SCHEMA links;
 
-CREATE TABLE link (
+CREATE TABLE links.link (
     link_id BIGINT PRIMARY KEY,
     url VARCHAR NOT NULL
 );
 
-CREATE TABLE chat (
+CREATE TABLE links.chat (
     chat_id BIGINT PRIMARY KEY,
     user_id BIGINT NOT NULL
 );
 
-CREATE TABLE link_chat (
+CREATE TABLE links.link_chat (
     link_id BIGINT NOT NULL,
     chat_id BIGINT NOT NULL,
     PRIMARY KEY (link_id, chat_id),
     FOREIGN KEY (link_id)
-        REFERENCES Link (link_id),
+        REFERENCES links.link (link_id),
     FOREIGN KEY (chat_id)
-        REFERENCES chat (chat_id)
+        REFERENCES links.chat (chat_id)
 );
