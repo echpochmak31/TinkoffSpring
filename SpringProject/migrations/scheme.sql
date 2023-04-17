f77e672e996d527391483b57dc199d54d983bca5
echpochmak31 <echpochmak83@gmail.com>
2023-04-17 17:33:07 +0300

    feat: alter db scheme

--- SpringProject/migrations/scheme.sql
+++ SpringProject/migrations/scheme.sql
@@ -19,3 +19,22 @@ CREATE TABLE links.link (
 --changeset gleb:2
 ALTER TABLE links.chat
 DROP COLUMN user_id;
+
+--changeset gleb:3
+
+DROP TABLE links.link;
+
+CREATE TABLE links.link (
+    link_id BIGSERIAL PRIMARY KEY,
+    url VARCHAR NOT NULL UNIQUE
+);
+
+CREATE TABLE links.link_chat (
+     link_id BIGINT NOT NULL,
+     chat_id BIGINT NOT NULL,
+     PRIMARY KEY(link_id, chat_id),
+     FOREIGN KEY (link_id)
+         REFERENCES links.link (link_id),
+     FOREIGN KEY (chat_id)
+         REFERENCES links.chat (chat_id)
+);
