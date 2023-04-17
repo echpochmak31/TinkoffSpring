8d79c2b686ff42ad9ee84ebdaf45bb66f2a253f5
echpochmak31 <echpochmak83@gmail.com>
2023-04-17 13:01:20 +0300

    feat: reformat db scheme

--- SpringProject/migrations/scheme.sql
+++ SpringProject/migrations/scheme.sql
@@ -3,22 +3,15 @@
 --changeset gleb:1
 CREATE SCHEMA links;
 
-CREATE TABLE links.link (
-    link_id BIGSERIAL PRIMARY KEY,
-    url VARCHAR NOT NULL
-);
-
 CREATE TABLE links.chat (
     chat_id BIGINT PRIMARY KEY,
     user_id BIGINT NOT NULL
 );
 
-CREATE TABLE links.link_chat (
-    link_id BIGINT NOT NULL,
+CREATE TABLE links.link (
     chat_id BIGINT NOT NULL,
-    PRIMARY KEY (link_id, chat_id),
-    FOREIGN KEY (link_id)
-        REFERENCES links.link (link_id),
+    url VARCHAR NOT NULL,
+    PRIMARY KEY (chat_id, url),
     FOREIGN KEY (chat_id)
         REFERENCES links.chat (chat_id)
 );
