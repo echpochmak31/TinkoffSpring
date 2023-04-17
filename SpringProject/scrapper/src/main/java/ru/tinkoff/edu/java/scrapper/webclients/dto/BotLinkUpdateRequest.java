3a9ec328ddecabcd9f376ae2b10fcde49bf3dc0d
echpochmak31 <echpochmak83@gmail.com>
2023-04-17 15:48:50 +0300

    feat: add BotHttpClient

---
+++ SpringProject/scrapper/src/main/java/ru/tinkoff/edu/java/scrapper/webclients/dto/BotLinkUpdateRequest.java
@@ -0,0 +1,22 @@
+package ru.tinkoff.edu.java.scrapper.webclients.dto;
+
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import lombok.NonNull;
+import org.springframework.validation.annotation.Validated;
+
+import java.net.URI;
+
+@Validated
+public record BotLinkUpdateRequest (
+        @NonNull
+        @Min(0)
+        @Max(Long.MAX_VALUE)
+        Long id,
+        @NonNull
+        URI url,
+        @NonNull
+        String description,
+        Long[] tgChatsIds
+) {
+}
