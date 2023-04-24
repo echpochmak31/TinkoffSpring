84d30bc034fa8760956f20bc0f097b5e174c659b
echpochmak31 <echpochmak83@gmail.com>
2023-04-24 21:14:26 +0300

    feat: add jpa database access type сщтвшÐconditional configuration

--- SpringProject/scrapper/src/main/java/ru/tinkoff/edu/java/scrapper/configuration/JpaAccessConfiguration.java
+++ SpringProject/scrapper/src/main/java/ru/tinkoff/edu/java/scrapper/configuration/JpaAccessConfiguration.java
@@ -4,7 +4,11 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import ru.tinkoff.edu.java.scrapper.dao.jpa.JpaChatRepository;
+import ru.tinkoff.edu.java.scrapper.dao.jpa.JpaLinkRepository;
+import ru.tinkoff.edu.java.scrapper.dao.jpa.JpaStackOverflowLinkRepository;
 import ru.tinkoff.edu.java.scrapper.services.jpa.JpaChatService;
+import ru.tinkoff.edu.java.scrapper.services.jpa.JpaLinkService;
+import ru.tinkoff.edu.java.scrapper.services.jpa.JpaStackOverflowLinkService;
 
 @Configuration
 @ConditionalOnProperty(prefix = "app", name = "database-access-type", havingValue = "jpa")
@@ -13,5 +17,15 @@ public class JpaAccessConfiguration {
     @Bean
     public JpaChatService jpaChatService(JpaChatRepository chatRepository) {
         return new JpaChatService(chatRepository);
+    }
+
+    @Bean
+    public JpaLinkService jpaLinkService(JpaLinkRepository linkRepository, JpaChatRepository chatRepository) {
+        return new JpaLinkService(linkRepository, chatRepository);
+    }
+
+    @Bean
+    public JpaStackOverflowLinkService jpaStackOverflowLinkService(JpaStackOverflowLinkRepository stackOverflowLinkRepository) {
+        return new JpaStackOverflowLinkService(stackOverflowLinkRepository);
     }
 }
