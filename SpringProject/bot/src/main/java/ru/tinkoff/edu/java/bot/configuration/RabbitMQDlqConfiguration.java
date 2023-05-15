5a552c445dfeb23625a2de9e74ba57fa71d85e22
echpochmak31 <echpochmak83@gmail.com>
2023-05-15 19:26:04 +0300

    refactor: codestyle after merge

--- SpringProject/bot/src/main/java/ru/tinkoff/edu/java/bot/configuration/RabbitMQDlqConfiguration.java
+++ SpringProject/bot/src/main/java/ru/tinkoff/edu/java/bot/configuration/RabbitMQDlqConfiguration.java
@@ -1,7 +1,11 @@
 package ru.tinkoff.edu.java.bot.configuration;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.amqp.core.*;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.QueueBuilder;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
